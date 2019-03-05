d955958fadcc5fa68a9d3e2abea49f0510c18b16
Juang Salaz Prabowo <juang.salaz.prabowo@gmail.com>
2019-03-05 17:22:36 +0700

    Initial commit

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PaymentController extends Controller
+{
+    public function paymentProcess()
+    {
+    	// Set your secret key: remember to change this to your live secret key in production
+		// See your keys here: https://dashboard.stripe.com/account/apikeys
+		\Stripe\Stripe::setApiKey("sk_test_A3NtT08DKuugEFBJTM1GuOop");
+
+		// Token is created using Checkout or Elements!
+		// Get the payment token ID submitted by the form:
+		$token = $_POST['stripeToken'];
+		$charge = \Stripe\Charge::create([
+		    'amount' => 10000,
+		    'currency' => 'usd',
+		    'description' => 'Example charge',
+		    'source' => $token,
+		]);
+    }
+
+    public function getPayment()
+    {
+    	echo 'test';
+    }
+}
