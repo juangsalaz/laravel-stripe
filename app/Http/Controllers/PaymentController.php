5daabb1704838c7bd9304e086ff351b66f83d2cf
Juang Salaz Prabowo <juang.salaz.prabowo@gmail.com>
2019-03-05 17:45:39 +0700

    Replace with your secreet key

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -10,7 +10,7 @@ class PaymentController extends Controller
     {
     	// Set your secret key: remember to change this to your live secret key in production
 		// See your keys here: https://dashboard.stripe.com/account/apikeys
-		\Stripe\Stripe::setApiKey("xxxx-xxxx-xxxx-xxxx");
+		\Stripe\Stripe::setApiKey("Replace with your secreet key");
 
 		// Token is created using Checkout or Elements!
 		// Get the payment token ID submitted by the form:
